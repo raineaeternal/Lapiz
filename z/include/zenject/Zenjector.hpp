486a60196019f7bb3acb44b93206c0fa115cb81f
dawnvt <64136899+dawnvt@users.noreply.github.com>
2022-02-02 17:19:13 +0100

    Make Zenjector header only

--- z/include/zenject/Zenjector.hpp
+++ z/include/zenject/Zenjector.hpp
@@ -3,25 +3,32 @@
 
 namespace z {
     class Zenjector {
-
+      public:
         /// @brief Installs a custom installer alongside another installer.
         template<class T>
-        void Install(T args[]);
+        void Install(T args[]) {
+            
+        };
 
         /// @brief Install bindings to a custom location with a backing installer(s).
-        void Install(z::Location);
+        void Install(z::Location) {
+
+        };
 
         /// @brief Install bindings to another installer without a custom installer
         template<class TKey>
-        void UseMetadataBinder(TKey);
+        void UseMetadataBinder(TKey) {
 
-        /// @brief Uses Ferns FMT packages on QPM. Not implemented, nor used currently.
-        // void UseLogger();
+        };
 
         /// @brief Lets you use Lapiz' HTTP service system. Not implemented, nor used currently.
-        // void UseHttpService();
+        void UseHttpService() {
 
-        /// @brief Makes it possible for Quest mods to sync to a github service. Not implemented, nor used currently.
-        // void UseLapizSync();
+        };
+
+        /// @brief Makes it possible for Quest mods to sync to an external service, like GitHub. Not implemented, nor used currently.
+        void UseLapizSync() {
+
+        };
     };
 }
