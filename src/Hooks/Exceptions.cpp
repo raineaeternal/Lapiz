c80a35a92242a58d9ec7fe2cc0ffd34097084ab9
Raine <64136899+raineio@users.noreply.github.com>
2022-08-29 16:12:03 +0200

    there was never a ".ctor_1"

--- src/Hooks/Exceptions.cpp
+++ src/Hooks/Exceptions.cpp
@@ -9,7 +9,7 @@ MAKE_AUTO_HOOK_FIND_INSTANCE(ZenjectException_ctor_1, classof(Zenject::ZenjectEx
     ZenjectException_ctor_1(self, exepMessage);
 }
 
-MAKE_AUTO_HOOK_FIND_INSTANCE(ZenjectException_ctor_2, classof(Zenject::ZenjectException*), ".ctor_1", void, Zenject::ZenjectException* self, StringW exepMessage) {
+MAKE_AUTO_HOOK_FIND_INSTANCE(ZenjectException_ctor_2, classof(Zenject::ZenjectException*), ".ctor", void, Zenject::ZenjectException* self, StringW exepMessage) {
     ERROR("ZenjectException: {}", exepMessage);
     ZenjectException_ctor_2(self, exepMessage);
 }
