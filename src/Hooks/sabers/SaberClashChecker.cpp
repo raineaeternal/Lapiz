f2a991015b7ea46ce8b66064aba9a51a8597e590
RedBrumbler <redbrumbler.official+github@gmail.com>
2022-08-30 22:20:39 +0200

    remove unused commented code

--- src/Hooks/sabers/SaberClashChecker.cpp
+++ src/Hooks/sabers/SaberClashChecker.cpp
@@ -45,32 +45,3 @@ MAKE_AUTO_HOOK_MATCH(TutorialInstaller_InstallBindings, &GlobalNamespace::Tutori
     typeArr[0] = il2cpp_utils::GetSystemType(klass);
     container->Bind<GlobalNamespace::SaberClashChecker*>()->To(typeArr)->AsSingle();
 }
-
-/*
-// TODO: test this scuffed hook which replaces the base game saberclash checker with ours
-template<>
-struct ::il2cpp_utils::il2cpp_type_check::MetadataGetter<static_cast<::Zenject::ConcreteIdBinderGeneric_1<::GlobalNamespace::SaberClashChecker*>* (::Zenject::DiContainer::*)(::Zenject::BindStatement*)>(&Zenject::DiContainer::Bind)> {
-    static const MethodInfo* get() {
-        static auto* contractType = &il2cpp_utils::GetClassFromName("Zenject", "BindStatement")->byval_arg;
-        static auto* genericParam = classof(::GlobalNamespace::SaberClashChecker*);
-        static auto* baseMinfo = ::il2cpp_utils::FindMethod(classof(Zenject::DiContainer*), "Bind", std::vector<Il2CppClass*>{genericParam}, ::std::vector<const Il2CppType*>{contractType});
-        return il2cpp_utils::MakeGenericMethod(baseMinfo, std::vector<Il2CppClass*>{genericParam});
-    }
-};
-
-struct Hook_DiContainer_Bind                                                                                                                            
-{                                                                                                                                              
-    using funcType = ::Zenject::ConcreteIdBinderGeneric_1<GlobalNamespace::SaberClashChecker*>* (*)(::Zenject::DiContainer* self, ::Zenject::BindStatement* bindStatement, const MethodInfo* minfo);                                                                                                  
-    static_assert(std::is_same_v<funcType, ::Hooking::InternalMethodCheck<decltype(static_cast<::Zenject::ConcreteIdBinderGeneric_1<::GlobalNamespace::SaberClashChecker*>* (::Zenject::DiContainer::*)(::Zenject::BindStatement*)>(&Zenject::DiContainer::Bind))>::funcType>, "Hook method signature does not match!");
-    constexpr static const char* name() { return "DiContainer_Bind"; }                                                                                     
-    static const MethodInfo* getInfo() { return ::il2cpp_utils::il2cpp_type_check::MetadataGetter<static_cast<::Zenject::ConcreteIdBinderGeneric_1<::GlobalNamespace::SaberClashChecker*>* (::Zenject::DiContainer::*)(::Zenject::BindStatement*)>(&Zenject::DiContainer::Bind)>::get(); }                              
-    static funcType* trampoline() { return &DiContainer_Bind; }                                                                                           
-    static inline ::Zenject::ConcreteIdBinderGeneric_1<GlobalNamespace::SaberClashChecker*>* (*DiContainer_Bind)(::Zenject::DiContainer* self, ::Zenject::BindStatement* bindStatement) = nullptr;                                                                                      
-    static funcType hook() { return hook_DiContainer_Bind; }                                                                                            
-    static ::Zenject::ConcreteIdBinderGeneric_1<GlobalNamespace::SaberClashChecker*>* hook_DiContainer_Bind(::Zenject::DiContainer* self, ::Zenject::BindStatement* bindStatement, const MethodInfo* minfo);                                                                                                   
-};                                                                                                                                             
-AUTO_INSTALL_ORIG(DiContainer_Bind)                                                                                                                       
-::Zenject::ConcreteIdBinderGeneric_1<GlobalNamespace::SaberClashChecker*>* Hook_DiContainer_Bind::hook_DiContainer_Bind(::Zenject::DiContainer* self, ::Zenject::BindStatement* bindStatement, const MethodInfo* minfo) {
-    return reinterpret_cast<::Zenject::ConcreteIdBinderGeneric_1<::GlobalNamespace::SaberClashChecker*>*>(self->Bind<Lapiz::Sabers::Effects::LapizSaberClashChecker*>(bindStatement));
-}
-*/
