1219f33e5c8e41c21d57b2460bd9c331540ddef3
Raine <64136899+raineio@users.noreply.github.com>
2022-09-12 09:07:36 +0200

    Final hopefully, untested

--- src/Hooks/Exceptions.cpp
+++ src/Hooks/Exceptions.cpp
@@ -4,12 +4,12 @@
 
 #include "Zenject/ZenjectException.hpp"
 
-MAKE_AUTO_HOOK_FIND_INSTANCE(ZenjectException_ctor_1, classof(Zenject::ZenjectException*), ".ctor", void,  Zenject::ZenjectException* self, StringW exceptionMessage) {
+MAKE_AUTO_HOOK_FIND_INSTANCE(ZenjectException_ctor_1, classof(::Zenject::ZenjectException*), ".ctor", void,  Zenject::ZenjectException* self, StringW exceptionMessage) {
     ERROR("ZenjectException: {}", exceptionMessage);
     ZenjectException_ctor_1(self, exepMessage);
 }
 
-MAKE_AUTO_HOOK_FIND_INSTANCE(ZenjectException_ctor_2, classof(Zenject::ZenjectException*), ".ctor", void, Zenject::ZenjectException* self, StringW exceptionMessage, System::Exception* innerException) {
-    ERROR("ZenjectException: {}", innerException->innerException);
+MAKE_AUTO_HOOK_FIND_INSTANCE(ZenjectException_ctor_2, classof(::Zenject::ZenjectException*), ".ctor", void, Zenject::ZenjectException* self, StringW exceptionMessage, System::Exception* innerException) {
+    ERROR("ZenjectException: {}", innerException->message->ToString());
     ZenjectException_ctor_2(self, exceptionMessage, innerException);
 }
