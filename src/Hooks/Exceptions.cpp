9509006ff6551f2b671a17f2b49adbdb5e753cc4
Raine <64136899+raineio@users.noreply.github.com>
2022-09-19 15:45:39 +0200

    fix build error

--- src/Hooks/Exceptions.cpp
+++ src/Hooks/Exceptions.cpp
@@ -6,7 +6,7 @@
 
 MAKE_AUTO_HOOK_FIND_INSTANCE(ZenjectException_ctor_1, classof(::Zenject::ZenjectException*), ".ctor", void,  Zenject::ZenjectException* self, StringW exceptionMessage) {
     ERROR("ZenjectException: {}", exceptionMessage);
-    ZenjectException_ctor_1(self, exepMessage);
+    ZenjectException_ctor_1(self, exceptionMessage);
 }
 
 MAKE_AUTO_HOOK_FIND_INSTANCE(ZenjectException_ctor_2, classof(::Zenject::ZenjectException*), ".ctor", void, Zenject::ZenjectException* self, StringW exceptionMessage, System::Exception* innerException) {
