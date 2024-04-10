252fd8e1a58fddcb65295c188cc83cb878884364
RedBrumbler <redbrumbler.official+github@gmail.com>
2024-04-10 15:10:23 +0200

    Small fixups for burnmark area hook

--- src/Hooks/sabers/SaberBurnMarkArea.cpp
+++ src/Hooks/sabers/SaberBurnMarkArea.cpp
@@ -38,20 +38,23 @@ MAKE_AUTO_HOOK_MATCH(SaberBurnMarkArea_Start, &GlobalNamespace::SaberBurnMarkAre
 
 // DOESN'T CALL ORIG ON PURPOSE
 MAKE_AUTO_HOOK_ORIG_MATCH(SaberBurnMarkArea_LateUpdate, &GlobalNamespace::SaberBurnMarkArea::LateUpdate, void, GlobalNamespace::SaberBurnMarkArea* self) {
-    if (self->_sabers[0])
+    if (self->_sabers && self->_sabers[0])
 	{
         // use sabers size as per TwoToLength
         for (int i = 0; i < self->_sabers.size(); i++)
         {
             UnityEngine::Vector3 zero{0, 0, 0};
-            bool flag = self->_sabers[i]->get_isActiveAndEnabled() && self->GetBurnMarkPos(self->_sabers[i]->saberBladeBottomPos, self->_sabers[i]->saberBladeTopPos, byref(zero));
+            auto saber = self->_sabers[i];
+            auto lineRenderer = self->_lineRenderers[i];
+
+            bool flag = saber->isActiveAndEnabled && self->GetBurnMarkPos(saber->saberBladeBottomPos, saber->saberBladeTopPos, byref(zero));
             if (flag && self->_prevBurnMarkPosValid[i]) {
                 UnityEngine::Vector3 vector = zero - self->_prevBurnMarkPos[i];
-                float magnitude = vector.get_magnitude();
+                float magnitude = vector.magnitude;
                 float num = 0.01f;
                 int num2 = (int)(magnitude / num);
                 int num3 = (num2 > 0) ? num2 : 1;
-                UnityEngine::Vector3 normalized = UnityEngine::Vector3(vector.z, 0.0f, -vector.x).get_normalized();
+                UnityEngine::Vector3 normalized = UnityEngine::Vector3(vector.z, 0.0f, -vector.x).normalized;
                 int num4 = 0;
                 while (num4 <= num3 && num4 < self->_linePoints.size()) {
         			UnityEngine::Vector3 vector2 = self->_prevBurnMarkPos[i] + vector * (float)num4 / (float)num3;
@@ -59,34 +62,42 @@ MAKE_AUTO_HOOK_ORIG_MATCH(SaberBurnMarkArea_LateUpdate, &GlobalNamespace::SaberB
                     self->_linePoints[num4] = self->WorldToCameraBurnMarkPos(vector2);
                     num4++;
                 }
-                self->_lineRenderers[i]->set_positionCount(num3 + 1);
-                self->_lineRenderers[i]->SetPositions(self->_linePoints);
-                self->_lineRenderers[i]->set_enabled(true);
+                lineRenderer->positionCount = num3 + 1;
+                lineRenderer->SetPositions(self->_linePoints);
+                lineRenderer->enabled = true;
             } else {
-                self->_lineRenderers[i]->set_enabled(false);
+                lineRenderer->enabled = false;
             }
+
             self->_prevBurnMarkPosValid[i] = flag;
             self->_prevBurnMarkPos[i] = zero;
         }
-        // turned into for loop as per DynamicUpdate
-        for (auto line : self->_lineRenderers) {
-            if (line->get_enabled()) {
-                self->_camera->Render();
-                break;
-            }
+
+        // check if any line renderers are enabled, and if so do the thing
+        bool anyEnabled = self->_lineRenderers.find_if([](auto l){ return l->enabled; }) != self->_lineRenderers.end();
+        if (anyEnabled) {
+            self->_camera->Render();
+            self->_disableBlitTimer = 0.0f;
+        } else {
+            self->_disableBlitTimer += UnityEngine::Time::get_deltaTime();
         }
-        self->_camera->set_targetTexture(self->_renderTextures[1]);
-        self->_renderer->get_sharedMaterial()->set_mainTexture(self->_renderTextures[1]);
-        self->_fadeOutMaterial->set_mainTexture(self->_renderTextures[0]);
-        self->_fadeOutMaterial->SetFloat(self->_fadeOutStrengthShaderPropertyID, std::max(0.0f, 1.0f - UnityEngine::Time::get_deltaTime() * self->_burnMarksFadeOutStrength));
-        UnityEngine::Graphics::Blit(self->_renderTextures[0], self->_renderTextures[1], self->_fadeOutMaterial);
 
+        if (self->_disableBlitTimer < 5.0f) { // after 5 seconds of no line renderers, blitting is no longer done (saves perf!)
+            auto renderTextures = self->_renderTextures;
+            self->_camera->targetTexture = renderTextures[1];
+            self->_renderer->sharedMaterial->mainTexture = renderTextures[1];
+            self->_fadeOutMaterial->mainTexture = renderTextures[0];
+            self->_fadeOutMaterial->SetFloat(self->_fadeOutStrengthShaderPropertyID, std::max(0.0f, 1.0f - UnityEngine::Time::get_deltaTime() * self->_burnMarksFadeOutStrength));
+            UnityEngine::Graphics::Blit(renderTextures[0], renderTextures[1], self->_fadeOutMaterial);
+            // swap textures
+            std::swap(renderTextures[0], renderTextures[1]);
+        }
+
+        auto latch = Lapiz::Sabers::Effects::SaberBurnMarkAreaLatch::get_instance();
+        if (latch) {
+            latch->SaberBurnMarkArea_LateUpdate_Postfix(self);
+        }
 	}
-
-    auto instance = Lapiz::Sabers::Effects::SaberBurnMarkAreaLatch::get_instance();
-    if (instance) {
-        instance->SaberBurnMarkArea_LateUpdate_Postfix(self);
-    }
 }
 
 MAKE_AUTO_HOOK_MATCH(SaberBurnMarkArea_OnEnable, &GlobalNamespace::SaberBurnMarkArea::OnEnable, void, GlobalNamespace::SaberBurnMarkArea* self) {
