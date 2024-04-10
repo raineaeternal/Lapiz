68531e944d0ffc50d706332a5c95784583597cc0
RedBrumbler <redbrumbler.official+github@gmail.com>
2024-04-10 20:33:58 +0200

    Fix expose crashing, should work properly now

--- include/zenject/internal/Mutators/MutatorManager.hpp
+++ include/zenject/internal/Mutators/MutatorManager.hpp
@@ -1,48 +1,12 @@
 #pragma once
 
-#include "utilities/logging.hpp"
-#include "utilities/typeutil.hpp"
-
 #include "../MutateSet.hpp"
-#include "Zenject/Context.hpp"
-#include "Zenject/SceneDecoratorContext.hpp"
-#include "Zenject/Context.hpp"
 #include "UnityEngine/MonoBehaviour.hpp"
 
 
 namespace Lapiz::Zenject::Internal::Mutators {
     class MutatorManager {
         public:
-            void Install(Internal::MutateSet* mutateSet, ::Zenject::Context* ctx, std::set<UnityEngine::MonoBehaviour*> iterlist) {
-                ::Zenject::SceneDecoratorContext* sceneDecoratorContext = il2cpp_utils::try_cast<::Zenject::SceneDecoratorContext>(ctx).value_or(nullptr);
-                if (!sceneDecoratorContext || !sceneDecoratorContext->m_CachedPtr) {
-                    return;
-                }
-
-                if (mutateSet->get_locationContractName() != sceneDecoratorContext->_decoratedContractName ||
-                    System::String::IsNullOrEmpty(sceneDecoratorContext->_decoratedContractName) ||
-                    mutateSet->get_locationContractName().empty()) {
-                    return;
-                }
-
-                if (iterlist.size() == 0) {
-                    ListW<UnityEngine::MonoBehaviour*> injectables(sceneDecoratorContext->_injectableMonoBehaviours);
-                    iterlist.insert(injectables.begin(), injectables.end());
-                }
-
-                UnityEngine::MonoBehaviour* toMutate = nullptr;
-                for (auto il : iterlist) {
-                    if (il->klass == mutateSet->get_typeToMutate()) {
-                        toMutate = il;
-                        break;
-                    }
-                }
-
-                if (toMutate && toMutate->m_CachedPtr) {
-                    mutateSet->get_onMutate()->Invoke(sceneDecoratorContext, toMutate);
-                } else {
-                    WARNING("Could not find {} in {}.", mutateSet->get_typeToMutate()->name, mutateSet->get_locationContractName());
-                }
-            };
+            void Install(Internal::MutateSet* mutateSet, ::Zenject::Context* ctx, std::set<UnityEngine::MonoBehaviour*> iterlist);
     };
 }
