5a7d8ba7c5cd7f1fe4bfc15180a0e567b9cda5ba
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2026-07-22 17:22:19 -0400

    Fix test

--- src/test/hooking_test.cpp
+++ src/test/hooking_test.cpp
@@ -15,10 +15,16 @@ DECLARE_CLASS_CODEGEN(Test, TestInjected, System::Object) {
   DECLARE_INJECT_FIELD(GlobalNamespace::SaberManager*, saberManager);
 };
 
+DEFINE_TYPE(Test, TestInjected);
+
+namespace Test {
+    void TestInjected::ctor() {}
+}
+
 MAKE_AFFINITY_HOOK_INJECTED(test2, ::Test::TestInjected*,
                             (i2c::metadata_getter<&GlobalNamespace::SaberClashEffect::Start>::method_info()), void, GlobalNamespace::SaberClashEffect* self) {
     auto saberManager = Injected->saberManager;
-    INFO("Injected value: {}", fmt::ptr(Injected));
+    INFO("Injected value: {}", fmt::ptr(Injected.ptr()));
     INFO("SaberManager: {}", fmt::ptr(saberManager));
 }
 
